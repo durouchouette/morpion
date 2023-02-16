18613546da42f8aa2b4a25f6e6a032184d47e8d9
Elodie Cari <elodiecari@elodies-mbp.mynetworksettings.com>
2023-02-16 15:33:21 -0500

    Setting up the UI

--- Morpion/ContentView.swift
+++ Morpion/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    @State private var plays: [Play?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.blue).opacity(0.6)
+                                .frame(width: geometry.size.width/3 - 15, height:geometry.size.width/3 - 15)
+                            
+                            Image(systemName: plays[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                        }
+                        .onTapGesture {
+                            plays[i] = Play(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Play {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
