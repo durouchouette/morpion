4f05072271ca22ac18798242046243cea8b16f09
Elodie Cari <elodiecari@elodies-mbp.mynetworksettings.com>
2023-02-16 15:07:37 -0500

    First commit

---
+++ Morpion/MorpionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorpionApp.swift
+//  Morpion
+//
+//  Created by Elodie Cari on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MorpionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
